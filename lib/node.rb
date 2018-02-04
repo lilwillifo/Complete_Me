a8cbe26132abd90fd7f8df5f49dad46f75a27b8f
jrambold <jrambold@gmail.com>
2018-02-04 08:37:26 -0700

    added select to node

--- lib/node.rb
+++ lib/node.rb
@@ -6,15 +6,53 @@ class Node
     @letter = letter
     @children = {}
     @word = false
+    @selections = []
   end
 
   def make_word
-    @word = true
+    if @word == false
+      @word = true
+    else
+      false
+    end
+  end
+
+  def delete_word
+    if @word == true
+      @word = false
+      true
+    else
+      false
+    end
   end
 
   def is_word?
     @word
   end
 
+  def select(word)
+    inserted = false
+    i = 0
+    while !inserted && i < @selections.length do
+      if @selections[i][0] == word
+        inserted = true
+        @selections[i][1] += 1
+        while @selections[i-1][1] < @selections[i][1] do
+          swap = @selections[i-1]
+          @selections[i-1] = @selections[i]
+          @selections[i] = swap
+        end
+      else
+        i +=1
+      end
+    end
+    if !inserted
+      @selections << [word, 1]
+    end
+  end
+
+  def get_selections
+    @selections.map {|selection| selection[0]}
+  end
 
 end
--- test/node_test.rb
+++ test/node_test.rb
@@ -14,6 +14,12 @@ class NodeTest < Minitest::Test
     refute node.is_word?
   end
 
+  def test_is_not_word_by_default
+    node = Node.new('a')
+
+    refute node.is_word?
+  end
+
   def test_make_word
     node = Node.new('a')
     node.make_word
@@ -21,5 +27,35 @@ class NodeTest < Minitest::Test
     assert node.is_word?
   end
 
+  def test_delete_word
+    node = Node.new('a')
+    node.make_word
 
+    assert node.is_word?
+
+    node.delete_word
+
+    refute node.is_word?
+  end
+
+  def test_select_and_get_selections
+    node = Node.new('a')
+
+    node.select("alpha")
+
+    assert_equal node.get_selections, ["alpha"]
+
+    node.select("alpha")
+
+    assert_equal node.get_selections, ["alpha"]
+
+    node.select("apple")
+    node.select("apple")
+
+    assert_equal node.get_selections, ["alpha","apple"]
+
+    node.select("apple")
+
+    assert_equal node.get_selections, ["apple","alpha"]
+  end
 end
